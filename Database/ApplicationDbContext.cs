bea656efb06bc0ecaddf739889470b61cbe70abe
kibethh <kibethh@gmail.com>
2025-10-22 01:16:38 +0300

    Init commit

---
+++ Database/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using DevHabit.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Database;
+
+public sealed class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<Habit> Habits { get; set; }
+    public DbSet<Tag> Tags { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.HasDefaultSchema(Schemas.Application);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+    }
+}
